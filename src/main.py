275bd31f5bedd773f42ffd3b79dbe4ffd6bc0906
Sarthak Negi <srnegi141@gmail.com>
2023-07-11 14:45:57 -0500

    final code to print all the unique movie names from all the endpoints in the form of a list

--- src/main.py
+++ src/main.py
@@ -13,27 +13,52 @@ URL_IMSDB = "https://imsdb.com/all-scripts.html"
 URL_DAILYSCRIPT_AM = "https://www.dailyscript.com/movie.html"
 URL_DAILYSCRIPT_NZ = "https://www.dailyscript.com/movie_n-z.html"
 
+FINAL_MOVIE_NAMES_LIST = []
+
 
 def main():
+    # SCREENPLAYS FOR YOU
     movie_names_screenplays_for_you = get_movie_names_screenplays_for_you(
         URL_SCREENPLAYS_FOR_YOU
     )
+    FINAL_MOVIE_NAMES_LIST.extend(movie_names_screenplays_for_you)
+
+    # SCREENPLAYS ONLINE
     movie_names_screenplays_online = get_movie_names_screenplays_online(
         URL_SCREENPLAYS_ONLINE
     )
+    FINAL_MOVIE_NAMES_LIST.extend(movie_names_screenplays_online)
+
+    # SCRIPT SLUG
     movie_names_script_slug = get_movie_names_script_slug(URL_SCRIPT_SLUG)
+    FINAL_MOVIE_NAMES_LIST.extend(movie_names_script_slug)
+
+    # AWESOME FILMS
     movie_names_awesome_film = get_movie_names_awesome_film(URL_AWESOME_FILM)
+    FINAL_MOVIE_NAMES_LIST.extend(movie_names_awesome_film)
+
+    # IMSDB
     movie_names_imsdb = get_movie_names_imsdb(URL_IMSDB)
+    FINAL_MOVIE_NAMES_LIST.extend(movie_names_imsdb)
+
+    # DAILY SCRIPT
     movie_names_daily_script_am = get_movie_names_daily_script(URL_DAILYSCRIPT_AM)
     movie_names_daily_script_mz = get_movie_names_daily_script(URL_DAILYSCRIPT_NZ)
     movie_names_daily_script = movie_names_daily_script_am + movie_names_daily_script_mz
+    FINAL_MOVIE_NAMES_LIST.extend(movie_names_daily_script)
 
-    print(f"\nSCREENPLAYS FOR YOU\n{movie_names_screenplays_for_you}")
-    print(f"\nSCREENPLAYS ONLINE\n{movie_names_screenplays_online}")
-    print(f"\nSCRIPT SLUG\n{movie_names_script_slug}")
-    print(f"\nAWESOME FILM\n{movie_names_awesome_film}")
-    print(f"\nIMSDB\n{movie_names_imsdb}")
-    print(f"\nDAILY SCRIPT\n{movie_names_daily_script}")
+    # print(f"\n\nSCREENPLAYS FOR YOU\n{movie_names_screenplays_for_you}")
+    # print(f"\n\nSCREENPLAYS ONLINE\n{movie_names_screenplays_online}")
+    # print(f"\n\nSCRIPT SLUG\n{movie_names_script_slug}")
+    # print(f"\n\nAWESOME FILM\n{movie_names_awesome_film}")
+    # print(f"\n\nIMSDB\n{movie_names_imsdb}")
+    # print(f"\n\nDAILY SCRIPT\n{movie_names_daily_script}")
+
+    # print(FINAL_MOVIE_NAMES_LIST)
+    print(len(FINAL_MOVIE_NAMES_LIST))
+    new_list = set(FINAL_MOVIE_NAMES_LIST)
+    print(len(new_list))
+    print(new_list)
 
 
 if __name__ == "__main__":
--- src/screenplays_for_you.py
+++ src/screenplays_for_you.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def get_movie_names_screenplays_for_you(URL: str) -> list[str]:
@@ -8,13 +9,20 @@ def get_movie_names_screenplays_for_you(URL: str) -> list[str]:
     doc = BeautifulSoup(page_html.text, "html.parser")
 
     movie_names = []
+    re_pattern = re.compile("\(\d{4}\)")
     movie_elements_list = doc.find("div", class_="two-thirds").find_all("p")
 
     del movie_elements_list[0]
 
     for element in movie_elements_list:
         name = element.find("a").string
+        name = re.sub(re_pattern, "", name).strip()
+
+        if name.endswith(", The"):
+            new_name = name.replace(", The", "")
+            name = "The " + new_name
+
         if name not in movie_names:
-            movie_names.append(name)
+            movie_names.append(name.strip())
 
     return movie_names
