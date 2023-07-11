181cdf71e72472bded550fb3f00946fe1a557bff
Sarthak Negi <srnegi141@gmail.com>
2023-07-11 11:31:29 -0500

    functionalized all the endpoints from my side

--- src/main.py
+++ src/main.py
@@ -3,11 +3,30 @@ from screenplays_online import get_movie_names_screenplays_online
 from script_slug import get_movie_names_script_slug
 
 URL_SCREENPLAYS_FOR_YOU = "https://sfy.ru/scripts"
+URL_SCREENPLAYS_ONLINE = "https://www.screenplays-online.de/"
+URL_SCRIPT_SLUG = "https://www.scriptslug.com/request/?pg="
 
 
 def main():
-    movie_names = get_movie_names_screenplays_for_you(URL_SCREENPLAYS_FOR_YOU)
-    print(movie_names)
+    movie_names_screenplays_for_you = get_movie_names_screenplays_for_you(
+        URL_SCREENPLAYS_FOR_YOU
+    )
+    movie_names_screenplays_online = get_movie_names_screenplays_online(
+        URL_SCREENPLAYS_ONLINE
+    )
+    movie_names_script_slug = get_movie_names_script_slug(URL_SCRIPT_SLUG)
+    print(
+        "----------**********----------**********--------------------**********----------**********----------"
+    )
+    print(f"SCREENPLAYS FOR YOU\n{movie_names_screenplays_for_you}")
+    print(
+        "----------**********----------**********--------------------**********----------**********----------"
+    )
+    print(f"SCREENPLAYS ONLINE\n{movie_names_screenplays_online}")
+    print(
+        "----------**********----------**********--------------------**********----------**********----------"
+    )
+    print(f"SCRIPT SLUG\n{movie_names_script_slug}")
 
 
 if __name__ == "__main__":
--- src/screenplays_online.py
+++ src/screenplays_online.py
@@ -1,13 +1,11 @@
-from typing import List
-
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.screenplays-online.de/"
-MOVIE_NAMES = []
 
-
-def get_movie_names_screenplays_online(doc: BeautifulSoup) -> List[str]:
+def get_movie_names_screenplays_online(URL: str) -> list[str]:
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64"}
+    page_html = requests.get(URL, headers=headers)
+    doc = BeautifulSoup(page_html.text, "html.parser")
     movie_names_list = []
     table_rows = doc.find("table", class_="screenplay-listing").find_all("tr")
     del table_rows[0]
@@ -23,16 +21,3 @@ def get_movie_names_screenplays_online(doc: BeautifulSoup) -> List[str]:
             movie_names_list.append(name)
 
     return movie_names_list
-
-
-def main():
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64"}
-    page_html = requests.get(URL, headers=headers)
-    doc = BeautifulSoup(page_html.text, "html.parser")
-
-    MOVIE_NAMES = get_movie_names_screenplays_online(doc)
-    print(MOVIE_NAMES)
-
-
-if __name__ == "__main__":
-    main()
