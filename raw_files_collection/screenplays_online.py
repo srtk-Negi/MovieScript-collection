947f5513a0caec1393f9504bc015c1ae363aa425
Sarthak Negi <srnegi141@gmail.com>
2023-07-14 17:33:33 -0500

    Updated the code for screenplays online

--- raw_files_collection/screenplays_online.py
+++ raw_files_collection/screenplays_online.py
@@ -8,7 +8,7 @@ re_year = re.compile("\(\d{4}\)")
 
 
 def get_raw_screenplays_online(URL: str) -> None:
-    """Function to get the name of the movie, year of release, link to the movie page and rawfile (html of the movie page)
+    """Function to get the name of the movie, link to the movie page and rawfile (html of the movie page)
 
     Args:
         URL (str): URL of the home page of 'Screenplays Online' website
@@ -24,51 +24,49 @@ def get_raw_screenplays_online(URL: str) -> None:
     )
     del table_rows[0]
 
+    title_link_map = {}
     for row in table_rows:
-        movie_1, year_1, movie_2, year_2 = get_movie_titles_and_years(row)
+        movie_1, movie_2 = get_movie_titles(row)
         link_1, link_2 = get_links_to_movie_pages(row, URL)
 
-        rawfile_1 = requests.get(link_1, headers=headers)
-        rawfile_html_1 = BeautifulSoup(rawfile_1.text, "html.parser")
-        filename_1 = get_filename(movie_1)
+        if movie_1:
+            title_link_map[movie_1] = link_1
+        if movie_2:
+            title_link_map[movie_2] = link_2
 
-        rawfile_2 = requests.get(link_2, headers=headers)
-        rawfile_html_2 = BeautifulSoup(rawfile_2.text, "html.parser")
-        filename_2 = get_filename(movie_2)
+    for movie_title, movie_page_link in title_link_map.items():
+        movie_title = re.sub(re_year, "", movie_title).strip()
 
-        with open(f"rawfiles/{filename_1}", "w", encoding="utf-8") as outfile:
-            outfile.write(str(rawfile_html_1))
+        if (
+            movie_title.endswith(", The")
+            or movie_title.endswith(", A")
+            or movie_title.endswith(", An")
+        ):
+            movie_title = switch_article(movie_title.split(" ")[-1], movie_title)
 
-        with open(f"rawfiles/{filename_2}", "w", encoding="utf-8") as outfile:
-            outfile.write(str(rawfile_html_2))
+        rawfile = requests.get(movie_page_link, headers=headers)
+        rawfile_html = BeautifulSoup(rawfile.text, "html.parser")
+        filename = get_filename(movie_title)
+
+        with open(f"rawfiles/{filename}", "w", encoding="utf-8") as outfile:
+            outfile.write(str(rawfile_html))
 
 
-def get_movie_titles_and_years(row: BeautifulSoup) -> tuple:
-    """Gets the movie names and years of release from the table row
+def get_movie_titles(row: BeautifulSoup) -> tuple:
+    """Gets the names of the movies from the table row
 
     Args:
         row (BeautifulSoup): A beautifulsoup object that is a table row
 
     Returns:
-        tuple: movie_1, year_1, movie_2, year_2
+        tuple: movie_1, movie_2
     """
     movies_in_row = row.find_all("td")
 
     movie_1 = movies_in_row[1].find("a").string
     movie_2 = movies_in_row[6].find("a").string
 
-    year_1, year_2 = get_year_of_release(movie_1, movie_2)
-
-    movie_1 = re.sub(re_year, "", movie_1).strip()
-    movie_2 = re.sub(re_year, "", movie_2).strip()
-
-    if movie_1.endswith(", The") or movie_1.endswith(", A") or movie_1.endswith(", An"):
-        movie_1 = switch_article(movie_1.split(" ")[-1], movie_1)
-
-    if movie_2.endswith(", The") or movie_2.endswith(", A") or movie_2.endswith(", An"):
-        movie_2 = switch_article(movie_2.split(" ")[-1], movie_2)
-
-    return movie_1, year_1, movie_2, year_2
+    return movie_1, movie_2
 
 
 def get_links_to_movie_pages(row: BeautifulSoup, URL: str) -> tuple:
@@ -87,29 +85,6 @@ def get_links_to_movie_pages(row: BeautifulSoup, URL: str) -> tuple:
     link_2 = URL + movies_in_row[6].find("a").get("href")
 
     return link_1, link_2
-
-
-def get_year_of_release(movie_1: str, movie_2: str) -> tuple:
-    """Gets the year of release of the movies (used as a helper function in get_movie_titles_and_years()
-
-    Args:
-        movie_1 (str): Name of the first movie
-        movie_2 (str): Name of the second movie
-
-    Returns:
-        tuple: year_1, year_2
-    """
-    try:
-        year_1 = re.findall(re_year, movie_1)[0][1:-1]
-    except IndexError:
-        year_1 = None
-
-    try:
-        year_2 = re.findall(re_year, movie_2)[0][1:-1]
-    except IndexError:
-        year_2 = None
-
-    return year_1, year_2
 
 
 def switch_article(article: str, movie_name: str) -> str:
