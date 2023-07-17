b7c88193eb77a5e932593d6d1d0782f99f49572a
Sarthak Negi <srnegi141@gmail.com>
2023-07-17 13:50:17 -0500

    Completed script_pdf.py

---
+++ raw_files_collection/script_pdf.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_raw_script_pdf(URL: str) -> None:
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    home_page_html = requests.get(URL, headers=headers)
+    home_page_data = BeautifulSoup(home_page_html.text, "html.parser")
+
+    sections = home_page_data.find("div", class_="entry-content").find_all("ul")
+    del sections[-1]
+
+    for section in sections:
+        li_tags = section.find_all("li")
+        for li_tag in li_tags:
+            movie_title = li_tag.find("a").text
+            link_to_pdf = li_tag.find("a").get("href")
+
+            if (
+                movie_title.endswith(", The")
+                or movie_title.endswith(", A")
+                or movie_title.endswith(", An")
+            ):
+                movie_title = switch_article(movie_title.split(" ")[-1], movie_title)
+
+            with open("rawfiles/scripts_pdf.txt", "a") as outfile:
+                outfile.write(f"{movie_title} - {link_to_pdf}\n")
+
+
+def switch_article(article: str, movie_name: str) -> str:
+    """Switches the position of the article of the movie name (The, An, A) from the end to the beginning (used as a helper function in get_movie_title_and_year())
+
+    Args:
+        article (str): The article of the movie name
+        movie_name (str): The movie name
+
+    Returns:
+        str: The movie name with the article at the beginning
+    """
+    new_name = movie_name.replace(f", {article}", "")
+    movie_name = f"{article} " + new_name
+
+    return movie_name
