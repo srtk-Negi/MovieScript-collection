348623cf884b861d1ad5032d93f7b4321b709da5
Poonacha Cheppudira <poonacha.cheppudira@utsa.edu>
2023-07-19 11:04:04 -0700

    Changed file path for raw file storage

--- raw_files_collection/daily_script.py
+++ raw_files_collection/daily_script.py
@@ -86,7 +86,7 @@ def get_raw_files_daily_script(URL_DAILY_SCRIPT: str) -> None:
             file_type = ".pdf"
             filename_2 = curate_filename(movie_title, file_type)
 
-            with open(f"rawfiles/{filename_2}", "wb") as f:
+            with open(f"F:\Movie-Data-Collection\Rawfiles{filename_2}", "wb") as f:
                 f.write(content)
                 pdf_count += 1
 
@@ -100,7 +100,7 @@ def get_raw_files_daily_script(URL_DAILY_SCRIPT: str) -> None:
             file_type = ".doc"
             filename_2 = curate_filename(movie_title, file_type)
 
-            with open(f"rawfiles/{filename_2}", "wb") as f:
+            with open(f"F:\Movie-Data-Collection\Rawfiles{filename_2}", "wb") as f:
                 f.write(content)
                 doc_count += 1
 
@@ -118,7 +118,9 @@ def get_raw_files_daily_script(URL_DAILY_SCRIPT: str) -> None:
             file_type = ".html"
             filename_2 = curate_filename(movie_title, file_type)
 
-            with open(f"rawfiles/{filename_2}", "w", encoding="utf-8") as f:
+            with open(
+                f"F:\Movie-Data-Collection\Rawfiles{filename_2}", "w", encoding="utf-8"
+            ) as f:
                 f.write(final_content)
                 txt_count += 1
 
@@ -133,7 +135,9 @@ def get_raw_files_daily_script(URL_DAILY_SCRIPT: str) -> None:
             file_type = ".html"
             filename_2 = curate_filename(movie_title, file_type)
 
-            with open(f"rawfiles/{filename_2}", "w", encoding="utf-8") as f:
+            with open(
+                f"F:\Movie-Data-Collection\Rawfiles{filename_2}", "w", encoding="utf-8"
+            ) as f:
                 f.write(str(soup))
                 rawfile_count += 1
 
