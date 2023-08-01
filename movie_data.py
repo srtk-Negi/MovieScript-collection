4a1df9a860ad8ace24df78f8b493afae24d6846b
Sarthak Negi <srnegi141@gmail.com>
2023-08-01 12:00:29 -0500

    screenplays online data collection complete

--- movie_data.py
+++ movie_data.py
@@ -2,13 +2,15 @@ from data_collector.awesome_film_data import get_awesome_film_scripts
 from data_collector.daily_script_data import get_daily_script_scripts
 from data_collector.imsdb_data import get_imsdb_scripts
 from data_collector.screenplays_for_you_data import get_screenplays_for_you_scripts
+from data_collector.screenplays_online_data import get_screenplays_online_scripts
 
 
 def main():
     # get_awesome_film_scripts()
     # get_daily_script_scripts()
     # get_imsdb_scripts()
-    get_screenplays_for_you_scripts()
+    # get_screenplays_for_you_scripts()
+    get_screenplays_online_scripts()
 
 
 if __name__ == "__main__":
