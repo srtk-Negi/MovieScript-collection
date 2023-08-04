f12fc81f7bd641707d929023185ed04c209827a0
Sarthak Negi <srnegi141@gmail.com>
2023-08-03 19:54:23 -0500

    OOP done for script pdf

--- raw_files_collector/script_pdf.py
+++ raw_files_collector/script_pdf.py
@@ -3,20 +3,18 @@ and write it to a file"""
 import requests
 from bs4 import BeautifulSoup
 import re
-
-# FILEPATH = "F:/Movie-Data-Collection/script_pdf"
-FILEPATH = "rawfiles"
+from movie import Movie
 
 
-def get_raw_script_pdf(URL: str) -> list[str]:
+def get_movies_script_pdf(URL: str) -> list[Movie]:
     """Function to get the name of the movie and the link to the pdf of the script from the 'Script PDFs' endpoint
 
     Args:
         URL (str): URL of the 'Script PDFs' endpoint
     """
-    MOVIE_NAMES = []
+    movies = []
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-    re_year = re.compile("\(\d{4}\)")
+    re_year = re.compile(r"\(\d{4}\)")
 
     try:
         home_page_html = requests.get(URL, headers=headers)
@@ -29,18 +27,17 @@ def get_raw_script_pdf(URL: str) -> list[str]:
     sections = home_page_data.find("div", class_="entry-content").find_all("ul")
     del sections[-1]
 
-    pdf_count = 0
     for section in sections:
         li_tags = section.find_all("li")
         for li_tag in li_tags:
             movie_title = li_tag.find("a").text
-            link_to_pdf = li_tag.find("a").get("href")
+            script_url = li_tag.find("a").get("href")
 
             try:
-                year = re.findall(re_year, movie_title)[0][1:-1]
+                movie_year = re.findall(re_year, movie_title)[0][1:-1]
                 movie_title = re.sub(re_year, "", movie_title).strip()
             except IndexError:
-                year = None
+                movie_year = None
 
             if (
                 movie_title.endswith(", The")
@@ -49,29 +46,11 @@ def get_raw_script_pdf(URL: str) -> list[str]:
             ):
                 movie_title = switch_article(movie_title.split(" ")[-1], movie_title)
 
-            try:
-                content = requests.get(link_to_pdf, headers=headers).content
-            except:
-                with open("error_log.txt", "a", encoding="utf-8") as outfile:
-                    outfile.write(
-                        f"Could not get {link_to_pdf} for {movie_title} from Script PDF\n"
-                    )
-                continue
+            movies.append(
+                Movie(title=movie_title, script_url=script_url, movie_year=movie_year)
+            )
 
-            MOVIE_NAMES.append(movie_title)
-
-            filename = ""
-            for ch in movie_title.lower():
-                if ch.isalnum() or ch == " ":
-                    filename += ch
-            filename_2 = "_".join(filename.strip().split()) + ".pdf"
-
-            with open(f"{FILEPATH}/{filename_2}", "wb") as outfile:
-                outfile.write(content)
-                pdf_count += 1
-
-    print(f"Total number of PDFs collected from 'Script PDFs': {pdf_count}")
-    return MOVIE_NAMES
+    return movies
 
 
 def switch_article(article: str, movie_name: str) -> str:
